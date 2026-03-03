3c7ba5fe9b70f84bfa14efa3974e754948bfedbf
Aswinradha93 <aswinpbc@gmail.com>
2026-03-04 00:17:15 +0530

    Add files via upload

---
+++ marathon/RedBusSearch.java
@@ -0,0 +1,91 @@
+package marathon;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RedBusSearch {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+
+		// Maximize the browser window.
+		driver.manage().window().maximize();
+
+		// Navigate to(https://www.redbus.in/)
+		driver.get("https://www.redbus.in/");
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		// Select the From source city.
+		driver.findElement(By.xpath("//div[contains(@class,'srcDestWrapper') and .//label[text()='From']]"))
+		.click();
+		//((JavascriptExecutor) driver).executeScript("arguments[0].click();", source);
+
+		driver.findElement(By.xpath("//div[contains(@class,'searchSuggestionWrapper')]//div[text()='Koyambedu']"))
+				.click();
+
+		driver.findElement(By.xpath("//div[contains(@class,'searchSuggestionWrapper')]//div[text()='Bangalore']"))
+				.click();
+		
+		// Select the To destination city.
+		driver.findElement(By.xpath("//div[contains(@class,'dateInputWrapper')]")).click();
+		Thread.sleep(1000);
+
+		// Select tomorrow’s travel date.
+		
+		//li[contains(@class,'date')]//span[text()='26']
+		driver.findElement(By.xpath("//div[contains(@class,'calendarDate') and @data-datetype='AVAILABLE']//span")).click();
+
+		// Click on the Search Buses button.
+		driver.findElement(By.xpath("//button[text()='Search buses']")).click();
+		Thread.sleep(1000);
+
+		// Capture and print the total number of buses displayed.
+		String count = driver
+				.findElement(By.xpath("//div[contains(@class,'textContainer')]//span[contains(@class,'subtitle')]"))
+				.getText();
+		System.out.println("**************Count before filter:" + count);
+
+		// Apply the AC Bus filter.
+		driver.findElement(By.xpath("//div[contains(@class,'mainContainer')]//div[contains(text(),'AC')]")).click();
+		Thread.sleep(1000);
+		count = driver
+				.findElement(By.xpath("//div[contains(@class,'textContainer')]//span[contains(@class,'subtitle')]"))
+				.getText();
+		System.out.println("**************Count after AC filter:" + count);
+
+		// Apply the High Rated bus filter under Bus Features.
+		driver.findElement(
+				By.xpath("//div[contains(@class,'mainContainer')]//div[contains(text(),'High Rated Buses')]")).click();
+		Thread.sleep(1000);
+		count = driver
+				.findElement(By.xpath("//div[contains(@class,'textContainer')]//span[contains(@class,'subtitle')]"))
+				.getText();
+		System.out.println("**************Count after High Rated filter:" + count);
+
+		// Capture all the bus prices displayed on the page.
+		List<WebElement> priceElements = driver.findElements(By.xpath("//p[contains(@class,'finalFare')]"));
+		List<Integer> prices = new ArrayList<>();
+
+		// Identify and print the lowest bus price.
+		for (WebElement e : priceElements) {
+			int price = Integer.parseInt(e.getText().replaceAll("[^0-9]", ""));
+			prices.add(price);
+		}
+		
+		prices.sort(null);
+		
+		System.out.println("**************Lowest price:" + prices.get(0));
+
+		// Print the title of the page.
+		System.out.println("**************Title:" + driver.getTitle());
+		driver.close();
+
+	}
+
+}
